1d676c97dd07f311adf64e5353f2ff9fadfbd57a
litianyi <2293641679@qq.com>
2026-07-25 09:25:44 +0800

    partner: 同步主站精简代码，移除photo字段

--- lib/partners.ts
+++ lib/partners.ts
@@ -2,8 +2,6 @@
   name: string;
   label: string;
   address: string;
-  /** 门头照片路径，放在 public/partners/ 目录下 */
-  photo: string | null;
   icon: "spark" | "orbit" | "grid";
 }
 
@@ -13,7 +11,6 @@ export const PARTNERS: Partner[] = [
     name: "九思教育",
     label: "EDUCATION SERVICES",
     address: "薛城区文景嘉园北区17号楼109门市",
-    photo: null,
     icon: "grid",
   },
 ];
