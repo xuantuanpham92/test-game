cb16668d1b4c9932c9d654f344ee4eb31001afe1
litianyi <2293641679@qq.com>
2026-07-25 09:25:26 +0800

    partner: 同步主站精简代码，移除photo字段

--- lib/partners.ts
+++ lib/partners.ts
@@ -2,8 +2,6 @@
   name: string;
   label: string;
   address: string;
-  /** 门头照片路径，放在 public/partners/ 目录下 */
-  photo: string | null;
   icon: "spark" | "orbit" | "grid";
 }
 
@@ -13,7 +11,6 @@ export const PARTNERS: Partner[] = [
     name: "金泽涮烤场",
     label: "DINING & GATHERING",
     address: "薛城区新城街道武夷山路东侧孵化园8厂房104室",
-    photo: null,
     icon: "grid",
   },
 ];
